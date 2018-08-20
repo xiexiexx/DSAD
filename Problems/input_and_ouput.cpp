92b27930f897e22beb629306c656dfb971ee7a0f
Xie Xie <DSAD2015@163.com>
2018-08-20 20:43:34 +0800

    Problems

---
+++ Problems/input_and_ouput.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int x;
+  cin >> x;               // 输入
+  const int N = 10000;    // 数组长度
+  char data[N];           // 字符数组
+  const int M = 26;       // 模
+  for (int i = 0; i < N; i++)
+  {
+    x = (x * x) % M;
+    data[i] = x + 'a';
+  }
+  cout << data[N - 1];    // 输出
+  return 0;
+}
---
+++ Problems/set_and_order.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+  const int d = 8;        // 学号共8位.
+  const int W = 10;       // 权值.
+  int x = 7;              // 随意指定.
+  set<int> S;
+  for (int i = 0; i < d; i++)
+  {
+    int num;
+    cin >> num;           // 依次输入你的学号, 每次输入1个数字
+    S.insert(W * i + num + d + x);
+    S.insert(- W * i - num - d - x);
+  }
+  for (auto iter = S.begin(); iter != S.end(); ++iter)
+    cout << *iter << " ";
+  cout << endl;
+  while (S.size() > 0)
+  {
+    cout << *S.rbegin() << " ";
+    S.erase(--S.end());
+  }
+  cout << endl;
+  return 0;
+}
