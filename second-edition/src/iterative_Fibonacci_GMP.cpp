f6a98178009fc8da14de14460ebd86a1f70a4ad2
Xie Xie <DSAD2015@163.com>
2021-01-21 11:58:24 +0800

    Create iterative_Fibonacci_GMP.cpp

---
+++ second-edition/src/iterative_Fibonacci_GMP.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+// 需要安装GMP库(https://gmplib.org). 
+#include "gmp.h"
+
+int main()
+{
+  // 变量分别代表f_{n - 2}, f_{n - 1}, f_{n}.
+  mpz_t m2, m1, f;
+  // 以字符串形式赋值m2和m1, 取10进制的0和1.
+  mpz_init_set_str(m2, "0", 10);
+  mpz_init_set_str(m1, "1", 10);
+  // 初始化f.
+  mpz_init(f);
+  size_t n = 2021;
+  for (size_t i = 2; i <= n; ++i)
+  {
+    mpz_add(f, m2, m1);
+    mpz_set(m2, m1);
+    mpz_set(m1, f);
+  }
+  std::cout << "Fibonacci " << n << std::endl;
+  gmp_printf("%Zd\n", f);
+  // 释放所有整数变量的空间, 注意列表要以NULL结尾.
+  mpz_clears(m2, m1, f, NULL);
+  return 0;
+}
