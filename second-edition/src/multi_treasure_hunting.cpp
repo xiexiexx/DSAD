0ff23ad54f22811d546141d5cfdfc7a5553330fc
Xie Xie <DSAD2015@163.com>
2021-01-28 23:42:09 +0800

    Update multi_treasure_hunting.cpp

--- second-edition/src/multi_treasure_hunting.cpp
+++ second-edition/src/multi_treasure_hunting.cpp
@@ -4,23 +4,24 @@
 using std::vector;
 using std::multiset;
 
-void TreasureHunting(const vector<vector<int>>& H, const vector<int>& F)
+void TreasureHunting(const vector<vector<int>>& H,
+                     const vector<int>& F)
 {
   // 存放待找宝物的多重集合S.
   multiset<int> S;
   // 将宝物逐个加入多重集合S中.
-  for (size_t i = 0; i < F.size(); i++)
-    S.insert( F[i] );
-  for (size_t i = 0; i < H.size(); i++)
+  for (const auto x : F)
+    S.insert(x);
+  for (size_t i = 0; i < H.size(); ++i)
   {
     // 若已无宝物可找则终止.
     if (S.empty())
       return;
     // 未退出则意味着仍有宝物可找.
-    for (size_t j = 0; j < H[i].size(); j++)
+    for (size_t j = 0; j < H[i].size(); ++j)
     {
       // 寻找山洞H[i]中的第j件宝物.
-      auto iter = S.find( H[i][j] );
+      auto iter = S.find(H[i][j]);
       if (iter != S.end())
       {
         cout << "Find " << *iter << endl;
