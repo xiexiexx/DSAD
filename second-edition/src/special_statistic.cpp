ae5192fc95008ef7b844ce8acfdfdff1fdfb14d8
Xie Xie <DSAD2015@163.com>
2021-01-28 23:16:27 +0800

    Create special_statistic.cpp

---
+++ second-edition/src/special_statistic.cpp
@@ -0,0 +1,62 @@
+#include <list>
+#include <iostream>
+
+using std::list;
+using std::cin;
+using std::cout;
+using std::endl;
+
+void input_preprocess(list<int>& L, int& m)
+{
+  L.clear();		// 清空L中所有数据.
+  int data;
+  std::cin >> data;
+  m = data;		// 注意空序列时m为负整数.
+  while (data >= 0)
+  {
+    // 使用push_back可以保留数据的输入次序.
+    L.push_back(data);
+    // 同时统计最大值.
+    if (data > m)
+      m = data;
+    cin >> data;	// 读入数据.
+  }
+}
+
+void print(const list<int>& L)
+{
+  // 由于此次遍历不改变链的状态, 所以使用常量迭代器citer.
+  // 常量迭代器可以保证对L的常量引用, 特别以cbegin和cend区别于begin和end.
+  // 此外要特别注意for语句中使用常量迭代器遍历的惯用法.
+  // 也可使用基于范围的for循环完成.
+  for (auto citer = L.cbegin(); citer != L.cend(); ++citer)
+    cout << *citer << ' ';
+}
+
+void mean_of_larger(list<int>& L, int d, int m)
+{
+  // 注意空序列时不能调用本函数, 其他情况下的自然数序列均有正确返回值.
+  // 计算阈值, 由于m最大, 只需大于等于阈值即可.
+  int threshold = m - d;
+  // 满足要求的数之总和, 以double型存储便于后续计算.
+  double sum = 0;
+  int n = 0;					// 满足要求的自然数个数.
+  // 使用auto定义迭代器iter, 注意可能利用它删除元素,
+  // 因为删除会改变容器状态, 所以iter是list<int>::iterator型的迭代器.
+  auto iter = L.begin();
+  while (iter != L.end())
+  {
+    if (*iter >= threshold)
+    {
+      sum += *iter;
+      n++;
+      ++iter;		// 迭代器向前.
+    }
+    else
+      iter = L.erase(iter);	// 删除后iter会被赋值到下一元素位置.
+  }
+  cout << "The mean of the larger numbers is " << sum / n << endl;
+  cout << "Larger numbers: ";
+  // 要求最后输出被保留的那些自然数, 所以需要再次遍历.
+  print(L);
+}
