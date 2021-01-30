1c7c9a9ec6a9026f3c94f6292a0d57897a80ddee
Xie Xie <DSAD2015@163.com>
2021-01-30 21:15:26 +0800

    Update cover.cpp

--- second-edition/src/cover.cpp
+++ second-edition/src/cover.cpp
@@ -1,14 +1,13 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-using namespace std;
 
 int main()
 {
-  string S = "Algorithm Design";
+  std::string S = "Algorithm Design";
   make_heap(S.begin(), S.end());
   sort_heap(S.begin(), S.end());
   auto D = unique(S.begin(), S.end());
-  cout << S.erase(D, S.end()) - S.begin();
+  std::cout << S.erase(D, S.end()) - S.begin();
   return 0;
 }
