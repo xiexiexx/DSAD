6d6df3f5e3b8f01374874892cebf4dfa45120192
Xie Xie <DSAD2015@163.com>
2021-02-02 13:49:41 +0800

    typo

--- second-edition/src/matrix.h
+++ second-edition/src/matrix.h
@@ -13,29 +13,29 @@ public:
   // 矩阵(i, j)位置的元素, 常量版本.
   const T& operator()(size_t i, size_t j) const;
   // 返回矩阵的行数.
-  size_t xsize() const
+  size_t x_size() const
   {
-    return nrows;
+    return n_rows;
   }
   // 返回矩阵的列数.
-  size_t ysize() const
+  size_t y_size() const
   {
-    return ncols;
+    return n_cols;
   }
 private:
   // 内部实现是一个向量的向量, 
   // 有兴趣的读者可测试sizeof(vector<T>)的值并思考.
   std::vector<std::vector<T>> M;
   // 矩阵的行数.
-  size_t nrows;
+  size_t n_rows;
   // 矩阵的列数.
-  size_t ncols;
+  size_t n_cols;
 };
 
 // 构造函数实现.
 template <typename T>
 matrix<T>::matrix(size_t p, size_t q, const T& value)
-  : M(p), nrows(p), ncols(q)
+  : M(p), n_rows(p), n_cols(q)
 {
   for (size_t i = 0; i < p; i++)
     M[i].resize(q, value);
