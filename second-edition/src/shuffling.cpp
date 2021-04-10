56f12be641010147f6dd16b6b536b9dc2f84c36d
Xie Xie <DSAD2015@163.com>
2021-04-10 14:07:21 +0800

    shuffle

--- second-edition/src/shuffling.cpp
+++ second-edition/src/shuffling.cpp
@@ -7,7 +7,7 @@
 // 注意还有shuffle_order_engine, 例如knuth_b.
 
 template <typename T>
-void shuffle(std::vector<T>& V)
+void shuffling(std::vector<T>& V)
 {
   // 随机种子为当前时间(已转换成unsigned int型), 也可用chrono的now函数.
   // 基于均匀分布uniform_int_distribution产生随机数,
@@ -15,14 +15,14 @@ void shuffle(std::vector<T>& V)
   unsigned int seed = static_cast<unsigned int>(time(NULL));
   std::default_random_engine generator(seed);
   std::uniform_int_distribution<size_t> distribution(0);
-  // 从V.size() - 1到1, 每次作随机交换,
-  // 不到0是因为那一次肯定无交换.
+  // 从V.size() - 1到1, 每次进行随机交换,
+  // 未取到0是因为那一次肯定没有交换.
   // 此外对于size_t型做减法并判断边界特别要注意避开负数!
   size_t i = V.size();
   // 注意此处的技巧, 也可直接写成while (i-- > 1)的语句.
   while (i > 1)
   {
-    i--;
+    --i;
     // 产生[0, i]内的随机整数.
     size_t j = distribution(generator) % (i + 1);
     // 交换V[i]和V[j].
