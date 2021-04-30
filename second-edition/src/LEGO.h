5736ed0a5a3c16a609c6d9b8771241b4dd1a435a
Xie Xie <DSAD2015@163.com>
2021-04-30 14:54:50 +0800

    Update LEGO.h

--- second-edition/src/LEGO.h
+++ second-edition/src/LEGO.h
@@ -17,8 +17,8 @@ public:
   void print(int width);
 private:
   // n为所求解底板当前边长, start为该底板左上角位置,
-  // spot为当前底板中已铺位置. 采用分治法DAC求解.
-  void DAC(size_t n, point start, point spot);
+  // singularity为当前底板中已铺位置. 采用分治法DAC求解.
+  void DAC(size_t n, point start, point singularity);
   matrix<size_t> baseplate; // 存储底板编号数据的矩阵.
   size_t length;            // 底板长度.
   size_t label;             // 编号, 为新铺砖标记编号且不断增加.
