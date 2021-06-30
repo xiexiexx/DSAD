a4288f2cf6cd863c88221a9847abc032752f1b3f
Xie Xie <DSAD2015@163.com>
2021-06-30 23:49:47 +0800

    Update DFS.cpp

--- second-edition/src/DFS.cpp
+++ second-edition/src/DFS.cpp
@@ -5,8 +5,7 @@ void digraph::DFS(size_t s)
   // 访问顶点s并置该顶点为已访问.
   visit(s);
   marked[s] = true;
-  // 对s所在链中的未访问顶点逐个进行递归搜索.
-  // 代码还可以写得更简洁一些, 留作练习.
+  // 对s所在链中的未访问顶点逐个进行递归搜索(代码还可以写得更简洁).
   for (auto citer = AL[s].cbegin(); citer != AL[s].cend(); ++citer)
     if (!marked[*citer])
       DFS(*citer);
